1bd612754e26576457a85d6570c180979ce94474
Jacob May <jacob.may@defenseunicorns.com>
2023-10-25 17:46:25 -0400

    fix output resource name

--- modules/postgres-profile-vm/outputs.tf
+++ modules/postgres-profile-vm/outputs.tf
@@ -1,4 +1,4 @@
 output "bootstrap_ip" {
   description = "IP address for the bootstrap virtual machines."
-  value       = nutanix_virtual_machine.rke2_bootstrap.nic_list_status.0.ip_endpoint_list[0]["ip"]
+  value       = nutanix_virtual_machine.postgres_profile_vm.nic_list_status.0.ip_endpoint_list[0]["ip"]
 }
