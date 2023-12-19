7ae290bd8e0ed3e824889a95f74f1f0dbf0737f9
Jacob May <jacob.may@defenseunicorns.com>
2023-12-19 16:40:58 -0500

    Expand docs and add example deployments

---
+++ examples/postgres-profile-vm/example.auto.tfvars
@@ -0,0 +1,8 @@
+nutanix_username    = "your-prism-central-user"
+nutanix_password    = "your-prism-central-password"
+nutanix_endpoint    = "prism-central.example.com"
+nutanix_port        = 9440
+nutanix_insecure    = true
+ssh_authorized_keys = ["ssh-rsa your ssh key here"]
+user_password       = "$6$eqCKlTML5rIALmwB$6fyOlrTK9E353ofDISHuEJxqIZx8MYt.mQM.qfbeydQX/CGnz204AlmYg5VCZ8O/xLKJ34CkgV7hyoUno08N9." # password123
+pg_password         = "password123"
---
+++ examples/rke2-cluster/main.tf
@@ -0,0 +1,41 @@
+
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = ">= 1.9.0"
+    }
+  }
+}
+
+provider "nutanix" {
+  username = var.nutanix_username
+  password = var.nutanix_password
+  endpoint = var.nutanix_endpoint
+  port     = var.nutanix_port
+  insecure = var.nutanix_insecure
+}
+
+resource "random_password" "example_token" {
+  length  = 40
+  special = false
+}
+
+module "example-cluster" {
+  source = "../modules/rke2"
+
+  nutanix_cluster     = "Your-Cluster"
+  nutanix_subnet      = "Your Subnet"
+  name                = "example-rke2"
+  server_count        = 3
+  agent_count         = 3
+  server_memory       = 16 * 1024
+  server_cpu          = 8
+  agent_memory        = 32 * 1024
+  agent_cpu           = 16
+  image_name          = "uds-rke2-rhel-some-build-tag"
+  ssh_authorized_keys = var.ssh_authorized_keys
+  server_dns_name     = "example-rke2.your-domain.com"
+  join_token          = random_password.example_token.result
+  bootstrap_cluster   = true
+}
