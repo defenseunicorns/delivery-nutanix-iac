944252670ceb4511271c26d24cbbe1541c160b42
Jacob May <jacob.may@defenseunicorns.com>
2024-08-02 23:09:52 +0000

    Add example of how the custom taints might be used to create a cluster with some agent nodes tainted

---
+++ examples/rke2-cluster-custom-taints/main.tf
@@ -0,0 +1,63 @@
+
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = ">= 1.9.0"
+    }
+  }
+}
+
+provider "nutanix" {
+  username = var.nutanix_username
+  password = var.nutanix_password
+  endpoint = var.nutanix_endpoint
+  port     = var.nutanix_port
+  insecure = var.nutanix_insecure
+}
+
+resource "random_password" "example_token" {
+  length  = 40
+  special = false
+}
+
+# Deploy cluster without custom taints
+module "example-cluster" {
+  source = "../modules/rke2"
+
+  nutanix_cluster     = "Your-Cluster"
+  nutanix_subnet      = "Your Subnet"
+  name                = "example-rke2"
+  server_count        = 3
+  agent_count         = 3
+  server_memory       = 16 * 1024
+  server_cpu          = 8
+  agent_memory        = 32 * 1024
+  agent_cpu           = 16
+  image_name          = "uds-rke2-rhel-some-build-tag"
+  ssh_authorized_keys = var.ssh_authorized_keys
+  server_dns_name     = "example-rke2.your-domain.com"
+  join_token          = random_password.example_token.result
+  bootstrap_cluster   = true
+}
+
+# Deploy and join additional agent nodes with custom taints to be used by gitaly
+module "gitaly-nodes" {
+  source = "../modules/rke2"
+
+  nutanix_cluster     = "Your-Cluster"
+  nutanix_subnet      = "Your Subnet"
+  name                = "example-gitaly-rke2"
+  server_count        = 0
+  agent_count         = 3
+  server_memory       = 16 * 1024
+  server_cpu          = 8
+  agent_memory        = 32 * 1024
+  agent_cpu           = 16
+  image_name          = "uds-rke2-rhel-some-build-tag"
+  ssh_authorized_keys = var.ssh_authorized_keys
+  server_dns_name     = "example-rke2.your-domain.com"
+  join_token          = random_password.example_token.result
+  bootstrap_cluster   = false
+  agent_custom_taints = ["gitaly-node=:NoSchedule"]
+}
